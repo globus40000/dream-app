739447613ab663aaed5361399cddba24d562dffa
globus40000 <globus40000@gmail.com>
2017-11-06 21:45:50 -0500

    [cli-stories] Using corporate proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,4 +1,6 @@
-module.exports = [
+const HttpsProxyAgent = require('https-proxy-agent');
+
+let proxyConfig = [
     {
         context: [
             "/google",
@@ -18,3 +20,19 @@ module.exports = [
         }
     }
 ];
+
+function setupForCorporateProxy(proxyConfig) {
+    let proxyServer = process.env.http_proxy || process.env.HTTP_PROXY;
+
+    if (proxyServer) {
+        let agent = new HttpsProxyAgent(proxyServer);
+        console.log('Using corporate proxy server: ' + proxyServer);
+        proxyConfig.forEach(function(entry) {
+            entry.agent = agent;
+        });
+    }
+
+    return proxyConfig;
+}
+
+module.exports = setupForCorporateProxy(proxyConfig);
